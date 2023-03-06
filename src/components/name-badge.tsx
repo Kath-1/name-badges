ddd8188c2df3766186e22c08f13def43deb27f9c
Karolin <karolin.thunman@gmail.com>
2023-03-06 10:07:38 +0100

    add ts types

--- src/components/name-badge.tsx
+++ src/components/name-badge.tsx
@@ -1,9 +1,14 @@
-const NameBadge = () => {
+type NameBadgeProps = {
+  greeting?: string; // string or undefined
+  name: string;
+};
+
+const NameBadge = ({ name, greeting }: NameBadgeProps) => {
   return (
     <section className="badge">
       <header className="badge-header">
-        <h1 className="text-5xl">HELLO</h1>
-        <p>My name is…</p>
+        <h1 className="text-5xl">{greeting}</h1>
+        <p>My name is {name}</p>
       </header>
       <div className="badge-body">
         <p className="badge-name">Steve</p>
